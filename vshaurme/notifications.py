89f5ee5a6387e7530e9800bcea0354e1bd2c6fed
alpden550 <alpden550@gmail.com>
2019-06-05 09:43:06 +0300

    Added subscription and comment notifications.

--- vshaurme/notifications.py
+++ vshaurme/notifications.py
@@ -9,12 +9,14 @@ def push_follow_notification(follower, receiver):
     message = _l('User <a href=\"%s\">%s</a> followed you.') % \
               (url_for('user.index', username=follower.username), follower.username)
     notification = Notification(message=message, receiver=receiver)
+    add_notification(notification)
 
 
 def push_comment_notification(photo_id, receiver, page=1):
     message = _l('<a href=\"%s#comments\">This photo</a> has new comment/reply.') % \
               (url_for('main.show_photo', photo_id=photo_id, page=page))
     notification = Notification(message=message, receiver=receiver)
+    add_notification(notification)
 
 
 def push_collect_notification(collector, photo_id, receiver):
@@ -23,3 +25,11 @@ def push_collect_notification(collector, photo_id, receiver):
                collector.username,
                url_for('main.show_photo', photo_id=photo_id))
     notification = Notification(message=message, receiver=receiver)
+    add_notification(notification)
+
+
+def add_notification(notification):
+    if notification:
+        db.session.add(notification)
+        db.session.commit()
+        db.session.close()
